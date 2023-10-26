d479bfa8a353b5dcc08203a296d041ec181d26ec
Abdirahman-ai <62496069+Abdirahman-ai@users.noreply.github.com>
2023-10-25 23:30:30 -0500

    Project 6 complete

--- app/src/main/java/com/example/randompet/MainActivity.kt
+++ app/src/main/java/com/example/randompet/MainActivity.kt
@@ -1,90 +1,61 @@
 package com.example.randompet
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
 
-    private var petImageURL = ""
-    private lateinit var petList: MutableList<String>
+    private lateinit var petList: MutableList<Pet>
     private lateinit var rvPets: RecyclerView
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getDogImageURL()
-        Log.d("petImageURL", "pet image URL set")
-
-        //  gets the button component
-        val button: Button = findViewById(R.id.petButton)
-        // update the ImageView to show the random image retrieved from the Dog API
-        val imageView: ImageView = findViewById(R.id.petImage)
-        getNextImage(button, imageView)
 
         petList = mutableListOf()
         rvPets = findViewById(R.id.pet_list)
 
+        val button: Button = findViewById(R.id.petButton)
+        button.setOnClickListener { refreshPetList() }
+
+        refreshPetList()
     }
 
-    // function to get an image URL from the dog API
-    private fun getDogImageURL(){
-        val client = AsyncHttpClient() // instantiate an AsyncHttpClient object
-
-        // set our client to get the data from  dog API
-        client["https://dog.ceo/api/breeds/image/random/20", object :JsonHttpResponseHandler() {
-            // The onSuccess() function executes if we are able to successfully call the API. The onFailure() function executes if there is an error.
+    private fun refreshPetList() {
+        val client = AsyncHttpClient()
+        val apiUrl = "https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_mfZkT8lsmYydy4F4DbBZO8egXQ8YwFzsFCx4WKCQWu0gBqg96jK5nJfgdnCJTE10"
+        client[apiUrl, object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
-                // get the URL for the image from the rest of the response
+                petList.clear() // Clear the existing list
                 if (json != null) {
-                    // retrieve the value for the key message from the JSON we obtain from the API as a String
-                    // and set our petImage variable to this value.
-                    //petImageURL = json.jsonObject.getString("message")
+                    val petArray = json.jsonArray
+                    for (i in 0 until petArray.length()) {
+                        val petObject = petArray.getJSONObject(i)
+                        val imageUrl = petObject.getString("url")
 
-                    val petImageArray = json.jsonObject.getJSONArray("message")
+                        val breedsArray = petObject.getJSONArray("breeds")
+                        val breedObject = breedsArray.getJSONObject(0)
+                        val name = breedObject.getString("name")
+                        val description = breedObject.getString("description")
 
-                    for(i in 0 until petImageArray.length()){
-                        petList.add(petImageArray.getString(i))
+                        petList.add(Pet(imageUrl, name, description))
                     }
                     val adapter = PetAdapter(petList)
                     rvPets.adapter = adapter
                     rvPets.layoutManager = LinearLayoutManager(this@MainActivity)
                 }
-                Log.d("Dog", "response successful$json")
             }
-            // The onFailure() function executes if there is an error in the API call
-            override fun onFailure(
-                statusCode: Int,
-                headers: Headers?,
-                response: String?,
-                throwable: Throwable?
-            ) {
-                if (response != null) {
-                    Log.d("Dog Error", response)
-                }
+
+            override fun onFailure(statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?) {
+                Log.d("Dog Error", response ?: "Unknown error")
             }
         }]
     }
-
-    private fun getNextImage(button: Button, imageView: ImageView){
-        // respond when the user taps the button.
-        // We want the getDogImageURL() function to be called when the button is tapped so we get a new random image,
-        // then use the Glide library to load this image into the ImageView.
-        button.setOnClickListener{
-            getDogImageURL()
-            Glide.with(this)
-                .load(petImageURL)
-                .fitCenter()
-                .into(imageView)
-        }
-    }
 }
+
--- app/src/main/java/com/example/randompet/petAdapter.kt
+++ app/src/main/java/com/example/randompet/petAdapter.kt
@@ -1,42 +1,43 @@
 package com.example.randompet
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.randompet.R
 
-/* Adapter class that extends RecyclerView.Adapter<PetAdapter.ViewHolder>() */
-class PetAdapter(private val petList: List<String>): RecyclerView.Adapter<PetAdapter.ViewHolder>() {
-    // class constructor so we can define a custom ViewHolder:
+data class Pet(
+    val imageUrl: String,
+    val name: String,
+    val description: String
+)
+
+class PetAdapter(private val petList: MutableList<Pet>) : RecyclerView.Adapter<PetAdapter.ViewHolder>() {
+
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val petImage: ImageView
-
-        init {
-            // Find our RecyclerView item's ImageView for future use
-            petImage = view.findViewById(R.id.pet_image)
-        }
+        val petImage: ImageView = view.findViewById(R.id.petItemImage)
+        val petName: TextView = view.findViewById(R.id.petItemName)
+        val petDescription: TextView = view.findViewById(R.id.petItemDescription)
     }
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        // Create a new view, which defines the UI of the list item
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.pet_item, parent, false)
-
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // Get element from the dataset at this position and replace the contents of the view with that element
+        val pet = petList[position]
         Glide.with(holder.itemView)
-            .load(petList[position])
+            .load(pet.imageUrl)
             .centerCrop()
             .into(holder.petImage)
+
+        holder.petName.text = pet.name
+        holder.petDescription.text = pet.description
     }
 
-    override fun getItemCount(): Int {
-        return petList.size
-    }
+    override fun getItemCount() = petList.size
 }
